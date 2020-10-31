35078d34cde6d1f9b86d15816da9446d2d5eba6e
harikrushna <hp52@njit.edu>
2020-10-31 15:39:22 -0400

    Task: addition module added in main Calculator file

---
+++ Calculator/Calculator.py
@@ -0,0 +1,12 @@
+from Calculator.addition import addition
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
